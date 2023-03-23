fdc10efd612dc414909cc26d8a52d3e662b6079d
Poor-Him <azeng2@vols.utk.edu>
2023-03-22 22:04:53 -0400

    Added dark mode and light mode in setting to allow user to change between the two.

--- app/src/main/java/com/example/cosc365/MainActivity.kt
+++ app/src/main/java/com/example/cosc365/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.cosc365
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+    private val PREFS_NAME = "MyPrefsFile"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Read saved night mode state from SharedPreferences
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val isDarkMode = sharedPref.getBoolean("dark_mode", false)
+
+        // Apply saved night mode state to app's theme
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
     fun openSecondActivity(view: View) {
         val intent = Intent(this, Settings::class.java)
--- app/src/main/java/com/example/cosc365/Settings.kt
+++ app/src/main/java/com/example/cosc365/Settings.kt
@@ -1,15 +1,50 @@
 package com.example.cosc365
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 
 class Settings : AppCompatActivity() {
+
+    private val PREFS_NAME = "MyPrefsFile"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // Read saved night mode state from SharedPreferences
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val isDarkMode = sharedPref.getBoolean("dark_mode", false)
+
+        // Apply saved night mode state to app's theme
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
     fun goBackToMain(view: View) {
         finish()
+    }
+
+
+
+    fun setLightMode(view: View) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        saveNightModeState(false)
+    }
+
+    fun setDarkMode(view: View) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        saveNightModeState(true)
+    }
+
+    private fun saveNightModeState(isDarkMode: Boolean) {
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putBoolean("dark_mode", isDarkMode)
+            apply()
+        }
     }
 }
